2e123997b9076df7cab95fb1085ec088d5696104
Abhi Hiremagalur <abhijit@hiremaga.com>
2014-04-20 12:29:12 -0700

    Fix typo

--- spec/integration/cookbook_spec.rb
+++ spec/integration/cookbook_spec.rb
@@ -14,8 +14,8 @@ describe 'sprout-ruby' do
   end
 
   it 'prevents `sudo gem`' do
-    plugin_path = "#{ENV['HOME']}/.bash_it/custom/gem_setup-warn_on_sudo.bash")
+    plugin_path = "#{ENV['HOME']}/.bash_it/custom/gem_setup-warn_on_sudo.bash"
     expect(File).to be_exists(plugin_path)
-    expect(File.read(plugin_path).to match(/Unalias gem at your own risk/)
+    expect(File.read(plugin_path)).to match(/Unalias gem at your own risk/)
   end
 end

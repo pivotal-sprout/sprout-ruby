fb74c17371b81895a4e5cfd8cbb71a506ad85d03
Abhi Hiremagalur <abhijit@hiremaga.com>
2014-04-20 11:57:07 -0700

    Make rubocop happy

--- spec/integration/cookbook_spec.rb
+++ spec/integration/cookbook_spec.rb
@@ -14,7 +14,8 @@ describe 'sprout-ruby' do
   end
 
   it 'prevents `sudo gem`' do
-    expect(File).to be_exists("#{ENV['HOME']}/.bash_it/custom/gem_setup-warn_on_sudo.bash")
-    expect(File.read("#{ENV['HOME']}/.bash_it/custom/gem_setup-warn_on_sudo.bash")).to match(/Unalias gem at your own risk/)
+    plugin_path = "#{ENV['HOME']}/.bash_it/custom/gem_setup-warn_on_sudo.bash")
+    expect(File).to be_exists(plugin_path)
+    expect(File.read(plugin_path).to match(/Unalias gem at your own risk/)
   end
 end

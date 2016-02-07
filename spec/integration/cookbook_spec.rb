49bef41731174e636be50dcf45ecdd848e0b4847
Dan Wendorf <dwendorf@pivotal.io>
2016-02-07 13:41:23 -0800

    Fix integration tests for local .gemrc installation

--- spec/integration/cookbook_spec.rb
+++ spec/integration/cookbook_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe 'sprout-ruby' do
   before :all do
-    expect(File).not_to be_exists('/etc/gemrc')
+    expect(File).not_to be_exists("#{ENV['HOME']}/.gemrc")
   end
 
   it 'converges without errors' do
     expect(system('soloist')).to eq(true)
   end
 
-  it 'creates a global gemrc' do
-    expect(File).to be_exists('/etc/gemrc')
+  it 'creates a local gemrc' do
+    expect(File).to be_exists("#{ENV['HOME']}/.gemrc")
   end
 
   it 'prevents `sudo gem`' do

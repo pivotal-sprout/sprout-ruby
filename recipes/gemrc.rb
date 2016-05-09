2fc7d5273fccd978321450b61088bbd608867dfc
Dan Wendorf <dwendorf@pivotal.io>
2016-05-08 18:20:13 -0700

    Use --no-document in .gemrc
    --no-rdoc and --no-ri are deprecated
    
    Resolves #4

--- recipes/gemrc.rb
+++ recipes/gemrc.rb
@@ -2,5 +2,5 @@ file "#{node['sprout']['home']}/.gemrc" do
   owner node['sprout']['user']
   mode '0644'
   action :create
-  content "install: --no-rdoc --no-ri\nupdate: --no-rdoc --no-ri\n"
+  content "install: --no-document\nupdate: --no-document\n"
 end
--- spec/unit/gemrc_spec.rb
+++ spec/unit/gemrc_spec.rb
@@ -6,7 +6,7 @@ describe 'sprout-ruby::gemrc' do
   it 'configures rubygems to skip RI and RDOC for the current user' do
     runner.converge(described_recipe)
 
-    expect(runner).to render_file('/home/fauxhai/.gemrc').with_content(/install: --no-rdoc --no-ri/)
-    expect(runner).to render_file('/home/fauxhai/.gemrc').with_content(/update: --no-rdoc --no-ri/)
+    expect(runner).to render_file('/home/fauxhai/.gemrc').with_content(/install: --no-document/)
+    expect(runner).to render_file('/home/fauxhai/.gemrc').with_content(/update: --no-document/)
   end
 end
